6db38050a0e38d69492f18a0e7a7abfdcc3ddcec
sanglantes <paul.holmberg@elev.ga.lbs.se>
2023-05-17 00:54:04 +0200

    Continued work on APIConfig widget

--- ApiConfig.py
+++ ApiConfig.py
@@ -14,17 +14,25 @@ class Ui_ApiConfig(object):
         ApiConfig.setObjectName("ApiConfig")
         ApiConfig.resize(413, 300)
         self.label = QtWidgets.QLabel(parent=ApiConfig)
-        self.label.setGeometry(QtCore.QRect(30, 10, 111, 41))
+        self.label.setGeometry(QtCore.QRect(30, 0, 91, 51))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label.setFont(font)
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(parent=ApiConfig)
-        self.label_2.setGeometry(QtCore.QRect(30, 50, 121, 41))
+        self.label_2.setGeometry(QtCore.QRect(30, 40, 131, 51))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.label_2.setFont(font)
         self.label_2.setObjectName("label_2")
-        self.pushButton = QtWidgets.QPushButton(parent=ApiConfig)
-        self.pushButton.setGeometry(QtCore.QRect(230, 250, 161, 41))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(parent=ApiConfig)
-        self.pushButton_2.setGeometry(QtCore.QRect(130, 250, 91, 41))
-        self.pushButton_2.setObjectName("pushButton_2")
+        self.register_button = QtWidgets.QPushButton(parent=ApiConfig)
+        self.register_button.setGeometry(QtCore.QRect(230, 250, 161, 41))
+        self.register_button.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+        self.register_button.setObjectName("register_button")
+        self.submit_button = QtWidgets.QPushButton(parent=ApiConfig)
+        self.submit_button.setGeometry(QtCore.QRect(130, 250, 91, 41))
+        self.submit_button.setCursor(QtGui.QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
+        self.submit_button.setObjectName("submit_button")
         self.lineEdit = QtWidgets.QLineEdit(parent=ApiConfig)
         self.lineEdit.setGeometry(QtCore.QRect(140, 10, 251, 32))
         self.lineEdit.setObjectName("lineEdit")
@@ -32,24 +40,39 @@ class Ui_ApiConfig(object):
         self.lineEdit_2.setGeometry(QtCore.QRect(140, 50, 251, 32))
         self.lineEdit_2.setObjectName("lineEdit_2")
         self.textBrowser = QtWidgets.QTextBrowser(parent=ApiConfig)
-        self.textBrowser.setGeometry(QtCore.QRect(20, 90, 371, 151))
+        self.textBrowser.setGeometry(QtCore.QRect(20, 120, 371, 121))
         self.textBrowser.setObjectName("textBrowser")
+        self.label_3 = QtWidgets.QLabel(parent=ApiConfig)
+        self.label_3.setGeometry(QtCore.QRect(70, 41, 16, 10))
+        self.label_3.setObjectName("label_3")
+        self.mirror_box = QtWidgets.QComboBox(parent=ApiConfig)
+        self.mirror_box.setGeometry(QtCore.QRect(257, 90, 131, 22))
+        self.mirror_box.setObjectName("mirror_box")
+        self.mirror_box.addItem("")
+        self.mirror_box.addItem("")
+        self.mirror_box.addItem("")
 
         self.retranslateUi(ApiConfig)
-        self.lineEdit_2.returnPressed.connect(self.pushButton_2.click) # type: ignore
-        self.lineEdit.returnPressed.connect(self.pushButton_2.click) # type: ignore
+        self.lineEdit_2.returnPressed.connect(self.submit_button.click) # type: ignore
+        self.lineEdit.returnPressed.connect(self.submit_button.click) # type: ignore
         QtCore.QMetaObject.connectSlotsByName(ApiConfig)
 
     def retranslateUi(self, ApiConfig):
         _translate = QtCore.QCoreApplication.translate
-        ApiConfig.setWindowTitle(_translate("ApiConfig", "Form"))
+        ApiConfig.setWindowTitle(_translate("ApiConfig", "API Setup"))
         self.label.setText(_translate("ApiConfig", "ChatGPT API key"))
         self.label_2.setText(_translate("ApiConfig", "RapidAPI key"))
-        self.pushButton.setText(_translate("ApiConfig", "Register RapidAI API key"))
-        self.pushButton_2.setText(_translate("ApiConfig", "Submit"))
+        self.register_button.setText(_translate("ApiConfig", "Register RapidAPI key"))
+        self.submit_button.setText(_translate("ApiConfig", "Submit"))
         self.textBrowser.setHtml(_translate("ApiConfig", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'Noto Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt;\">Connection details:</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">[<span style=\" color:#00557f;\">*</span>] Awaiting...</p></body></html>"))
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:\'Segoe UI\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'Noto Sans\'; font-size:12pt;\">Connection details:</span></p></body></html>"))
+        self.label_3.setText(_translate("ApiConfig", "or"))
+        self.mirror_box.setItemText(0, _translate("ApiConfig", "RapidAPI main"))
+        self.mirror_box.setItemText(1, _translate("ApiConfig", "RapidAPI mirror (1)"))
+        self.mirror_box.setItemText(2, _translate("ApiConfig", "RapidAPI mirror (2)"))
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from PyQt6.QtWidgets import *
 from ApiConfig import Ui_ApiConfig
-
-def verify_api_key(api_key):
-    if len(api_key) != 50:
-        return "ERROR: Invalid API key"
+from PyQt6.QtCore import QThread, pyqtSignal
+import requests
+from time import sleep
 
 class APIConfig(QWidget):
     def __init__(self, *args, **kwargs):
@@ -11,7 +10,131 @@ class APIConfig(QWidget):
 
         self.ui = Ui_ApiConfig()
         self.ui.setupUi(self)
+        self.setFixedSize(413, 300)
+
+        self.ui.submit_button.clicked.connect(self.start_verification)
+        self.ui.register_button.clicked.connect(self.open_website)
+        self.ui.mirror_box.currentIndexChanged.connect(self.handle_combo_box)
+
+        self.worker_thread = WorkerThread()
+        self.worker_thread.response_received.connect(self.handle_response)
+
         self.show()
+
+    def handle_combo_box(self, index):
+        urls = {
+            0: "https://chatgpt53.p.rapidapi.com/",
+            1: "https://openai80.p.rapidapi.com/chat/completions",
+            2: "https://chatgpt-api6.p.rapidapi.com/standard-gpt",
+        }
+        payloads = {
+        # CHATGPT FORK
+            0: {
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": "Echo."
+                    }
+                ]
+            },
+        # OFFICIAL CHATGPT
+            1: {
+                "model": "gpt-3.5-turbo",
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": "Echo."
+                    }
+                ]
+            },
+        # CHATGPT FORK
+            2: {
+                "conversation": [
+                    {
+                        "role": "user",
+                        "content": "Echo."
+                    }
+                ]
+            },
+        }
+        headers = {
+            0: {
+                "content-type": "application/json",
+                "X-RapidAPI-Key": self.ui.lineEdit_2.text().strip(),
+                "X-RapidAPI-Host": "chatgpt53.p.rapidapi.com"
+            },
+            1: {
+                "content-type": "application/json",
+                "X-RapidAPI-Key": self.ui.lineEdit_2.text().strip(),
+                "X-RapidAPI-Host": "openai80.p.rapidapi.com"
+            },
+            2: {
+                "content-type": "application/json",
+                "X-RapidAPI-Key": self.ui.lineEdit_2.text().strip(),
+                "X-RapidAPI-Host": "chatgpt-api6.p.rapidapi.com"
+            },
+        }
+        self.url = urls[index]
+        self.payload = payloads[index]
+        self.headers = headers[index]
+
+    def open_website(self):
+        import webbrowser
+        webbrowser.open("https://rapidapi.com/auth/sign-up")
+
+    def start_verification(self):
+        lineEdit_focus = self.ui.lineEdit.hasFocus()
+        lineEdit_2_focus = self.ui.lineEdit_2.hasFocus()
+
+        if lineEdit_2_focus or not len(self.ui.lineEdit.text().strip()):
+            api_key = self.ui.lineEdit_2.text()
+            self.ui.textBrowser.append("INFO: Verifying key (this uses a prompt)...")
+            self.worker_thread.set_api_key(api_key)
+            self.worker_thread.start()
+
+        elif lineEdit_focus or not len(self.ui.lineEdit_2.text().strip()):
+            api_key = self.ui.lineEdit.text()
+            print(api_key)
+        else:
+            self.ui.textBrowser.append("ERROR: Multiple forms filled")
+
+    def handle_response(self, response):
+        if response.status_code != 200:
+            self.ui.textBrowser.append(f"ERROR: Received response status {response.status_code}")
+            message = response.json().get('message')
+            if message:
+                self.ui.textBrowser.append(f"INFO: {message} (try a mirror)")
+        else:
+            self.ui.textBrowser.append("INFO: Successfully verified key")
+
+class WorkerThread(QThread):
+    response_received = pyqtSignal(requests.Response)
+
+    def __init__(self):
+        super().__init__()
+        self.api_key = ""
+
+    def set_api_key(self, api_key):
+        self.api_key = api_key
+
+    def run(self):
+        url = "https://chatgpt53.p.rapidapi.com/"
+        payload = {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "Echo."
+                }
+            ]
+        }
+        headers = {
+            "content-type": "application/json",
+            "X-RapidAPI-Key": self.api_key.strip(),
+            "X-RapidAPI-Host": "chatgpt53.p.rapidapi.com"
+        }
+        response = requests.post(url, json=payload, headers=headers)
+        self.response_received.emit(response)
+
 
 if __name__ == "__main__":
     import sys
